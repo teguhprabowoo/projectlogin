ba47d56af11bb0d30448f5082b6513ce83af1412
teguhprabowoo <teguhprabowo458@mail.com>
2025-09-26 16:31:01 +0700

    project_login

--- login.php
+++ login.php
@@ -1,3 +1,28 @@
+<?php
+session_start();
+
+// Cek apakah user sudah login
+if (isset($_SESSION['username'])) {
+    header("Location: dashboard.php");
+    exit;
+}
+
+// Proses login saat form dikirim
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'] ?? '';
+    $password = $_POST['password'] ?? '';
+
+    // Login sederhana (username: admin, password: 123)
+    if ($username === 'bowo' && $password === '123') {
+        $_SESSION['username'] = $username;
+        $_SESSION['role'] = 'Mahasiswa';
+        header("Location: dashboard.php");
+        exit;
+    } else {
+        $error = "Username atau password salah!";
+    }
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,9 +30,10 @@
 </head>
 <body>
 <h2>Form Login</h2>
+
 <?php if (!empty($error)) echo "<p style='color:red;'>$error</p>"; ?>
 
-<form method="post" action="proses_login.php">
+<form method="post">
     Username: <input type="text" name="username" required><br><br>
     Password: <input type="password" name="password" required><br><br>
     <button type="submit">Login</button>
